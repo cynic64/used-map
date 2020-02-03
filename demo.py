087d55cff64c088b141cd9ba667d0c94257056d0
Nicholas Schweitzer <nicholas_schweitzer@FVFYW8HBJ1WT.mis-munich.de>
2020-02-03 13:45:01 +0100

    initial commit

---
+++ demo.py
@@ -0,0 +1,15 @@
+import pygame as pg
+
+pg.init()
+
+surface = pygame.display.set_mode((400, 300))
+
+pygame.display.set_caption('Hello World!')
+
+while True: # main game loop
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
